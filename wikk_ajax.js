3b846cb8d88b99e6a8133be1ed409c0d800eea3a
robertburrowes <r.burrowes@auckland.ac.nz>
2017-07-04 18:31:48 +1200

    Removed default params, as old browser broke
    Also added VERSION, for tracking.
    
    Older browsers didn't like function(a = 1) style default value declarations.

--- wikk_ajax.js
+++ wikk_ajax.js
@@ -1,4 +1,8 @@
 var wikk_ajax = ( function() {
+
+  var VERSION = "1.0.1";
+  function version() { return VERSION; }
+
   //Default error callback is an alert
   //  @param jqXHR [Object]
   //  @param textStatus [String] Error code
@@ -17,8 +21,12 @@ var wikk_ajax = ( function() {
   //  @param response_type [String] The available data types are text, html (Default), xml, json, jsonp, and script
   //  @param stringify [Boolean] stringify ? JSON.stringify(args) : $.param(args)
   //  @param delay [Integer] Millisecond delay before ajax call is made.
-  function delayed_ajax_post_call(url, args, callback, error_callback=ajax_error, completion_callback=null, response_type='html', stringify=false, delay=180000) {
-      setTimeout(function () {ajax_post_call(url , args, callback, error_callback, completion_callback, response_type, stringify); }, delay );
+  function delayed_ajax_post_call(url, args, callback, error_callback, completion_callback, response_type, stringify, delay) {
+    if(response_type == null) response_type='html';
+    if(stringify == null) stringify=false;
+    if(error_callback == null) error_callback=ajax_error;
+    if(delay == null) delay = 180000;
+    setTimeout(function () {ajax_post_call(url , args, callback, error_callback, completion_callback, response_type, stringify); }, delay );
   }
 
 
@@ -31,8 +39,12 @@ var wikk_ajax = ( function() {
   //  @param completion_callback [Function] Called when the request finishes (after success and error callbacks are executed).  Function( jqXHR jqXHR, String textStatus )
   //  @param response_type [String] The available data types are text, html (Default), xml, json, jsonp, and script
   //  @param delay [Integer] Millisecond delay before ajax call is made.
-  function delayed_ajax_get_call(url,args,callback, error_callback=ajax_error, completion_callback=null, response_type='html', traditional=false, delay=180000) {
-      setTimeout(function () {ajax_get_call(url , args, callback, error_callback, completion_callback, response_type, traditional); }, delay );
+  function delayed_ajax_get_call(url,args,callback, error_callback, completion_callback, response_type, traditional, delay) {
+    if(response_type == null) response_type='html';
+    if(traditional == null) traditional=false;
+    if(error_callback == null) error_callback=ajax_error;
+    if(delay == null) delay = 180000;
+    setTimeout(function () {ajax_get_call(url , args, callback, error_callback, completion_callback, response_type, traditional); }, delay );
   }
 
 
@@ -44,7 +56,10 @@ var wikk_ajax = ( function() {
   //  @param completion_callback [Function] Called when the request finishes (after success and error callbacks are executed).  Function( jqXHR jqXHR, String textStatus )
   //  @param response_type [String] The available data types are text, html (Default), xml, json, jsonp, and script
   //  @param stringify [Boolean] stringify ? JSON.stringify(args) : $.param(args)
-  function ajax_post_call(url, args, callback, error_callback=ajax_error, completion_callback=null, response_type='html', stringify=false) {
+  function ajax_post_call(url, args, callback, error_callback, completion_callback, response_type, stringify) {
+    if(response_type == null) response_type='html';
+    if(stringify == null) stringify=false;
+    if(error_callback == null) error_callback=ajax_error;
     var serial_args = stringify ? JSON.stringify(args) : $.param(args);
     var content_type = stringify ? "application/json; charset=UTF-8" : "application/x-www-form-urlencoded; charset=UTF-8";
     var newDataRequest = $.ajax( url, {
@@ -67,7 +82,10 @@ var wikk_ajax = ( function() {
   //  @param error_callback [Function] Called if the request fails. Function( jqXHR jqXHR, String textStatus, String errorThrown )
   //  @param completion_callback [Function] Called when the request finishes (after success and error callbacks are executed).  Function( jqXHR jqXHR, String textStatus )
   //  @param response_type [String] The available data types are text, html (Default), xml, json, jsonp, and script
-  function ajax_get_call(url, args, callback, error_callback=ajax_error, completion_callback=null, response_type='html', traditional=false) {
+  function ajax_get_call(url, args, callback, error_callback, completion_callback, response_type, traditional) {
+    if(response_type == null) response_type='html';
+    if(traditional == null) traditional=false;
+    if(error_callback == null) error_callback=ajax_error;
     var serial_args = $.param(args,traditional);
     var newDataRequest = $.ajax( url, {
          type: 'GET', //default behaviour
@@ -89,6 +107,7 @@ var wikk_ajax = ( function() {
     delayed_ajax_get_call: delayed_ajax_get_call,
     ajax_post_call: ajax_post_call,
     ajax_get_call: ajax_get_call,
-    ajax_error: ajax_error
+    ajax_error: ajax_error,
+    version: version
   };
 })();

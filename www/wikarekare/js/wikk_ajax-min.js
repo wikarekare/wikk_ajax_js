7b6051a18cfb2b4e85136c967a604526be40c9fd
robertburrowes <r.burrowes@auckland.ac.nz>
2023-04-17 13:58:43 +1200

    generated

--- www/wikarekare/js/wikk_ajax-min.js
+++ www/wikarekare/js/wikk_ajax-min.js
@@ -28,12 +28,12 @@ var newDataRequest = $.ajax( url, {
 type: 'POST',
 data: serial_args,
 contentType: content_type,
-dataType: response_type, 
+dataType: response_type,
 cache: false,
-timeout: 3600000, 
+timeout: 3600000,
 success: callback ,
 error: error_callback == null ? ajax_error : error_callback,
-complete: completion_callback 
+complete: completion_callback
 });
 }
 function ajax_get_call(url, args, callback, error_callback, completion_callback, response_type, traditional) {
@@ -42,15 +42,15 @@ if(traditional == null) traditional=false;
 if(error_callback == null) error_callback=ajax_error;
 var serial_args = $.param(args,traditional);
 var newDataRequest = $.ajax( url, {
-type: 'GET', 
+type: 'GET',
 data: serial_args, processData: false,
-dataType: response_type == null ? 'html' : response_type, 
-contentType: "application/x-www-form-urlencoded; charset=UTF-8", 
-timeout: 3600000, 
+dataType: response_type == null ? 'html' : response_type,
+contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+timeout: 3600000,
 cache: false,
-success: callback , 
-error: error_callback == null ? ajax_error : error_callback, 
-complete: completion_callback 
+success: callback ,
+error: error_callback == null ? ajax_error : error_callback,
+complete: completion_callback
 });
 }
 return {
